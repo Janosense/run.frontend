c94e0a3541fbbba4102197f02cd3eb338d133b5f
Janosense <janosense@gmail.com>
2019-11-01 13:41:32 +0200

    Added about page.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,5 @@
 import device from 'current-device'
-import init_sticky_main_nav from "./modules/init-sticky-main-nav";
+import init_sticky_main_nav from './modules/init-sticky-main-nav';
 
 init_sticky_main_nav();
 
